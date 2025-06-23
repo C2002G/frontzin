67a0edc33109a0343626ee05857349ee140bb1bb
C2002G <claudiogabriel2002@gmail.com>
2025-06-23 13:12:41 -0300

    23/06

---
+++ desafio1/src/Components/TituloDinamico.js
@@ -0,0 +1,19 @@
+class TituloDinamico extends HTMLElement{
+    constructor(){
+        super();
+
+
+        const shadow = this.attachShadow({mode: "open"});
+
+
+
+        //base do component
+
+        //estilizar o component
+
+
+        //enviar para a shadow
+    }
+}
+
+customElements.define('titulo-dinamico', TituloDinamico)

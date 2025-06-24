20b07a23eefeb88d833d2d88f0c8ad341b5ce0b0
C2002G <claudiogabriel2002@gmail.com>
2025-06-24 14:31:00 -0300

    finalizado

--- desafio1/src/Components/CardNews.js
+++ desafio1/src/Components/CardNews.js
@@ -32,7 +32,7 @@ class CardNews extends HTMLElement {
     cardRight.setAttribute("class", "card__right");
 
     const newsImage = document.createElement("img");
-    newsImage.src = this.getAttribute("photo") || "assets/photo-default.jpg"
+    newsImage.src = this.getAttribute("photo") || "assets/photo-default.jpg";
 
     cardRight.appendChild(newsImage);
 
@@ -42,7 +42,48 @@ class CardNews extends HTMLElement {
     return componentRoot;
   }
 
-  styles() {}
+  styles() {
+    const style = document.createElement("style");
+    style.textContent = `
+        
+        .card {
+        justify-content: space-between;
+        width: 100%; /*pegar 100% da tela*\
+        
+        /* https://cssgenerator.org/box-shadow-css-generator.html */
+        box-shadow: 11px 16px 13px 10px rgba(135, 122, 122, 0.65);
+        -webkit-box-shadow: 11px 16px 13px 10px rgba(135, 122, 122, 0.65);
+        -moz-box-shadow: 11px 16px 13px 10px rgba(135, 122, 122, 0.65);
+        display: flex;
+        /* deixa uma do lado da outra */
+        flex-direction: row;
+        }
+
+        .card_left {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        padding-left: 10px;
+        }
+
+        .card_left > a {
+        margin-top: 15px;
+        font-size: 25px;
+        color: black;
+        text-decoration: none;
+        font-weight: bold;
+        }
+
+        .card_left > p {
+        color: gray;
+        }
+        .card_left > span {
+        font-weight: 400;
+        }
+    `;
+
+    return style;
+  }
 }
 //sempre colocar o define com nome com traço
 customElements.define("card-news", CardNews);

8e4fd96bdb4b341c25b60b9c9d21a3776568286e
C2002G <claudiogabriel2002@gmail.com>
2025-06-24 10:45:30 -0300

    a foi

--- desafio1/src/Components/TituloDinamico.js
+++ desafio1/src/Components/TituloDinamico.js
@@ -8,12 +8,20 @@ class TituloDinamico extends HTMLElement{
 
 
         //base do component
-
+        const componentRoot = document.createElement("h1");
+        componentRoot.textContent = this.getAttribute("titulo");
         //estilizar o component
-
+        const style = document.createElement("style");
+        style.textContent = `
+            h1{
+                color: red;
+            }
+        `;
 
         //enviar para a shadow
+        shadow.appendChild(componentRoot);
+        shadow.appendChild(style);
     }
 }
 
-customElements.define('titulo-dinamico', TituloDinamico)
+customElements.define('titulo-dinamico', TituloDinamico);
